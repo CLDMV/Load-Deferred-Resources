c54189d2abdec70a14e32906c124e9303086afca
Shinrai <tronox@hotmail.com>
2019-02-01 16:51:18 -0800

    Fix Safari not liking to play nice with innerHTML

--- ldr.js
+++ ldr.js
@@ -1,10 +1,10 @@
 /*!
-  * loadDeferredResources v1.0.0 (https://github.com/CLDMV/Load-Deferred-Resources)
-  * Copyright 2019 The loadDeferredResources Authors (https://github.com/CLDMV/Load-Deferred-Resources/graphs/contributors)
-  * Licensed under GPL-3.0 (https://github.com/CLDMV/Load-Deferred-Resources/blob/master/LICENSE)
+  * loadDeferredResources v1.0.0 ( https://github.com/CLDMV/Load-Deferred-Resources )
+  * Copyright 2019 The loadDeferredResources Authors ( https://github.com/CLDMV/Load-Deferred-Resources/graphs/contributors )
+  * Licensed under GPL-3.0 ( https://github.com/CLDMV/Load-Deferred-Resources/blob/master/LICENSE )
   */
 (function (l, o, a, d, u) {
-	function loadDeferredResources() {
+	function ldr() {
 		var nodes = document.getElementsByClassName(d);
 		if (nodes.length > 0) {
 			var cleanup_nodes = [];
@@ -12,9 +12,11 @@
 				var node = nodes[i];
 				cleanup_nodes.push(node);
 				var replacement = document.createElement("div");
-				replacement.setAttribute("class", d + "deferred-resources-loaded");
-				replacement.innerHTML = node.innerHTML;
-				var deferred_scripts = replacement.getElementsByTagName('script');
+				replacement.setAttribute("class", d + "-loaded");
+				// childNodes[0].nodeValue used instead of innerHTML due to safari bug
+				replacement.innerHTML = node.childNodes[0].nodeValue;
+				// replacement.innerHTML = node.innerHTML;
+				var deferred_scripts = replacement.getElementsByTagName(a);
 				if (deferred_scripts.length > 0) {
 					var scripts = [];
 					for (b = 0; b < deferred_scripts.length; b++) {
@@ -24,44 +26,44 @@
 						for (c = 0; c < attributes.length; c++) {
 							var attribute = attributes[c];
 							script_data[attribute.nodeName] = attribute.nodeValue;
-						}
-						if (typeof script_data.src === "undefined") {
+						};
+						if (typeof script_data.src === u) {
 							script_data.innerHTML = deferred_script.innerHTML;
-						}
+						};
 						script_data.deferred_scripts = deferred_script;
 						scripts.push(script_data);
-					}
+					};
 					for (b = 0; b < scripts.length; b++) {
 						var script_data = scripts[b];
 						replacement.removeChild(script_data.deferred_scripts);
 						script_data.deferred_scripts = null;
-						var script = document.createElement('script');
+						var script = document.createElement(a);
 						for (var key in script_data) {
 							if (script_data.hasOwnProperty(key)) {
 								if (script_data[key] !== null) {
 									script[key] = script_data[key];
 								}
 							}
-						}
-						if (typeof script.type === "undefined") {
+						};
+						if (typeof script.type === u) {
 							script.type = 'text/javascript';
-						}
+						};
 						replacement.appendChild(script);
 					}
-				}
+				};
 				document.body.appendChild(replacement);
-			}
+			};
 			// Cleanup
 			for (i = 0; i < cleanup_nodes.length; i++) {
 				var node = cleanup_nodes[i];
 				node.parentElement.removeChild(node);
-			}
+			};
 		}
 	};
 	var raf = requestAnimationFrame||mozRequestAnimationFrame||webkitRequestAnimationFrame||msRequestAnimationFrame;
 	if (raf) {
-		raf(function(){window.setTimeout(loadDeferredResources,0);});
+		raf(function(){window.setTimeout(ldr,0);});
 	} else {
-		window.addEventListener('load', loadDeferredResources);
-	}
+		window.addEventListener('load', ldr);
+	};
 })(window, document, 'script', 'deferred-resources', 'undefined');
